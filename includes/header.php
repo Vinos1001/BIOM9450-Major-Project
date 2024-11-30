b96db6fc2b5d637b6488b459b8e9531b0d1bf55e
Alice Gao <alice.gao@student.unsw.edu.au>
2024-11-30 13:35:03 +1100

    Fixing up inconsistent style

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,15 @@
+<script>
+    function buttonNav(page) {
+        location.href = page;
+    }    
+</script>
+
 <nav class='nav-header'>
     <img src="../includes/Logo.png" style="width:62px;height:50px;">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <div class='navbar-button'><a href="dashboard.php">Dashboard</a></div>
-    <div class='navbar-button'><a href="add_patient.php">Add Patient</a></div>
-    <div class='navbar-button'><a href="about.php">About Us</a></div>
+    <div class='navbar-button' onclick="buttonNav('dashboard.php')">Dashboard</div>
+    <div class='navbar-button' onclick="buttonNav('add_patient.php')">Add Patient</div>
+    <div class='navbar-button' onclick="buttonNav('about.php')">About Us</div>
     <div class="logged-in-info">
         <a>Logged in as <strong><?php echo htmlspecialchars($username); ?></strong></a>
     </div>
